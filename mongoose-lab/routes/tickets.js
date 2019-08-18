a186fb82a339ef1b10268f9336ec2462a441bd00
Jared Rodriguez <jaredarodriguez@gmail.com>
2019-08-17 21:37:21 -0500

    add to ticket controller

--- mongoose-lab/routes/tickets.js
+++ mongoose-lab/routes/tickets.js
@@ -2,4 +2,8 @@ var express = require("express");
 var router = express.Router();
 var ticketsCtrl = require("../controllers/tickets");
 
+router.get("/tickets/new", ticketsCtrl.new);
+router.post("/tickets", ticketsCtrl.create);
+router.post("/flights/:id/tickets", ticketsCtrl.addToTicket);
+
 module.exports = router;
